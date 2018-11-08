3151588ad041783fb4e593342c0a3a131cdd3cb0
vansharyan1719 <44702309+vansharyan1719@users.noreply.github.com>
2018-11-08 17:18:01 +0530

    Add files via upload

---
+++ Ex2.py
@@ -0,0 +1,5 @@
+# comment, this is so you can read your programe later.
+# Anything after this sign is ignored by Python.
+print("I could have code like this") # and comments like this.
+# print("This will not print")
+Print("This will print")
---
+++ Ex5.py
@@ -0,0 +1,20 @@
+name = "Piyush Pankaj"
+age = 31 #that's my real age.!!!!!!!Seriously.!!!!!!
+height = 5.10 #yes you can measure it.
+weight = 62 # Yes I am under weight !!!!
+pounds = weight * 2.20462
+eyes = "Black" # Unfortunately
+teeth = "White" # I hope so..
+hair = "Black" # But I love red hair and had red hair in college!! Seriously
+
+print(f"Lets talk about {name}")
+print(f"He's {height} inch tall.")
+print(f"He's {weight} heavy.")
+print("Actually that's not that heavy.")
+print(f"He has got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending upon number of smoke.")
+
+#Now the tricky part
+total = age + height + weight
+print(f"IF I add {age}, {height} and {weight}, I will get {total}")
+print(f"My weight in pounds would be {pounds} lbs.")
