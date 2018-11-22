dd1f8680d0f5e5ff7dcf148a20ca545c23d7d806
Pankaj <piyush.pankaj@capgemini.com>
2018-11-22 17:54:44 +0530

    Printing, Printing, Printing

---
+++ Ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJul\nAug"
+print("Here are the days:", days)
+print("Here are the moths:", months)
+
+print("""
+We
+can type
+as many
+line as
+we want
+""")
