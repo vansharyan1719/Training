20afb71e6a2bb8bf61b8d2724ce8106e8cc4bf3d
Pankaj <piyush.pankaj@capgemini.com>
2018-11-22 17:53:55 +0530

    Printing and Printing

---
+++ Ex8.py
@@ -0,0 +1,6 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format("true", "False", "True", "False"))
+print(formatter.format(formatter, formatter, formatter, formatter))
