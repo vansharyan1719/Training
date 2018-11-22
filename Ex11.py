6d404219db607b737a4a05a7633308a6951d159a
Pankaj <piyush.pankaj@capgemini.com>
2018-11-22 18:35:26 +0530

    Asking Questions.

---
+++ Ex11.py
@@ -0,0 +1,8 @@
+print("How odl are you ??", end = '')
+age =  input()
+print("How tall are you??", end = '')
+height = input()
+print("How much do you weight", end = '')
+weight = input()
+
+print(f"So you're {age} years old, {height} inch tall and {weight} kg heavy.")
